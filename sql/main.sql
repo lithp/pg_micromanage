6786cfbd293987216422e9824f91405b136ae025
Brian Cloutier <github@briancloutier.com>
2017-02-10 20:34:49 +0300

    Add test for boolean operations

--- sql/main.sql
+++ sql/main.sql
@@ -1,5 +1,6 @@
 CREATE EXTENSION micromanage;
 
+-- SELECT a FROM a;
 \set buf `cat example-messages/select-a-from-a.msg | protoc queries.proto --encode=SelectQuery | base64 -w0`
 SELECT * FROM run_select(:'buf');
 
@@ -9,14 +10,22 @@ INSERT INTO a VALUES (10);
 
 SELECT * FROM run_select(:'buf');
 
+-- SELECT d FROM a;
 \set buf `cat example-messages/select-d-from-a.msg | protoc queries.proto --encode=SelectQuery | base64 -w0`
 SELECT * FROM run_select(:'buf');
 
+-- this is not expressable in sql, it should definitely fail!
 \set buf `cat example-messages/select-d-from-bad-index.msg | protoc queries.proto --encode=SelectQuery | base64 -w0`
 SELECT * FROM run_select(:'buf');
 
+-- SELECT a, 10 FROM a;
 \set buf `cat example-messages/select-const-from-a.msg | protoc queries.proto --encode=SelectQuery | base64 -w0`
 SELECT * FROM run_select(:'buf');
 
+-- SELECT a + 10 FROM a;
 \set buf `cat example-messages/select-a-plus-10.msg | protoc queries.proto --encode=SelectQuery | base64 -w0`
+SELECT * FROM run_select(:'buf');
+
+-- SELECT a, a > 5 FROM a;
+\set buf `cat example-messages/select-a-gt-5.msg | protoc queries.proto --encode=SelectQuery | base64 -w0`
 SELECT * FROM run_select(:'buf');
